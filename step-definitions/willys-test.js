d1ed8037658a75454c4b0970a1501b5f3868351b
Noruh <89393382+Noruh@users.noreply.github.com>
2021-10-01 11:34:42 +0200

    User story 3
    Feature+Step-definitions

---
+++ step-definitions/willys-test.js
@@ -0,0 +1,58 @@
+const { $, $$ } = require('../helpers/element-selection.js');
+
+module.exports = function () {
+
+  this.Given(/^that I am on "([^"]*)"$/, async function (url) {
+    await helpers.loadPage(url);
+  });
+
+  this.When(/^I enter "([^"]*)" in the search box$/, async function (searchPhrase) {
+    let searchBox = await $('input[placeholder="Sök i e-handeln"]');
+    await searchBox.sendKeys(searchPhrase);
+  });
+
+  this.When(/^press the enter key$/, async function () {
+    let searchBox = await $('input[placeholder="Sök i e-handeln"]');
+    await searchBox.sendKeys(selenium.Key.ENTER);
+  });
+
+  //Add product button - not working. Second button?
+  this.When(/^I click on the plus button on a product$/, async function () {
+    let clickButton = await $$('.CardQuantityInputField_quantity-button__3ohGY');
+    let confirmButton = clickButton[1];
+    await confirmButton.click();
+  });
+
+  this.Then(/^I should get a search result$/, async function () {
+    await driver.wait(until.elementsLocated(by.css('.Grid_grid__1YmC6')), 20000);
+    let findList = await $('.Grid_grid__1YmC6');
+    expect(findList).to.not.equal(null);
+  });
+
+  this.Then(/^the search result should contain the correct product$/, async function () {
+    let findResults = await $$('.Product_product-name__1IyPc');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.include('Satsumas Klass 1');
+  });
+
+  //not working
+  this.Then(/^the product should be added to the shopping cart$/, async function () {
+    let findResults = await $$('.MiniCartstyles__StyledCounter-sc-1wu749y-6 cQZUCU');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.not.equal(0);
+  });
+
+};
+
+
+
+
+
+
+
