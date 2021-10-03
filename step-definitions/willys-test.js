352fbc66a11dd39673ade39fb1d1ee328db2a2fa
Noruh <89393382+Noruh@users.noreply.github.com>
2021-10-03 19:35:08 +0200

    User story 5
    Ändra antalet av en vara i varukorgen
    
    Feature + javascript

--- step-definitions/willys-test.js
+++ step-definitions/willys-test.js
@@ -6,6 +6,32 @@ module.exports = function () {
     await helpers.loadPage(url);
   });
 
+  this.Given(/^the product is added to the shopping cart$/, async function () {
+    let findResults = await $$('.MiniCartstyles__StyledCounter-sc-1wu749y-6 cQZUCU');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.not.equal(0);
+  });
+
+  this.Given(/^the shopping cart menu is open$/, async function () {
+    let clickButton = await $('.egACAL');
+    await clickButton.click();
+  });
+
+  this.Given(/^I click the cookies button$/, async function () {
+    await helpers.loadPage('https://www.willys.se/sok?q=satsumas');
+    let clickButton = await $$('#onetrust-consent-sdk #onetrust-accept-btn-handler');
+    let confirmButton = clickButton[0];
+    await confirmButton.click();
+  });
+
+  this.Given(/^I click the "till kassan"-button$/, async function () {
+    let clickButton = await $('.cCYiYM');
+    await clickButton.click();
+  });
+
   this.When(/^I enter "([^"]*)" in the search box$/, async function (searchPhrase) {
     let searchBox = await $('input[placeholder="Sök i e-handeln"]');
     await searchBox.sendKeys(searchPhrase);
@@ -16,9 +42,23 @@ module.exports = function () {
     await searchBox.sendKeys(selenium.Key.ENTER);
   });
 
-  //Add product button - not working. Second button?
+
   this.When(/^I click on the plus button on a product$/, async function () {
     let clickButton = await $$('.CardQuantityInputField_quantity-button__3ohGY');
+    let confirmButton = clickButton[1];
+    await confirmButton.click();
+  });
+
+  this.When(/^I click on the plus button on the product in the shopping cart$/, async function () {
+    await helpers.loadPage('https://www.willys.se/sok?q=satsumas');
+    let clickButton = await $$('.CartQuantityInputField_quantity-button__RMmnb');
+    let confirmButton = clickButton[0];
+    await confirmButton.click();
+  });
+
+  this.When(/^I click on the minus button on the product in the shopping cart$/, async function () {
+    await helpers.loadPage('https://www.willys.se/sok?q=satsumas');
+    let clickButton = await $$('.CartQuantityInputField_quantity-button__RMmnb');
     let confirmButton = clickButton[1];
     await confirmButton.click();
   });
@@ -38,7 +78,7 @@ module.exports = function () {
     expect(resultTexts).to.include('Satsumas Klass 1');
   });
 
-  //not working
+
   this.Then(/^the product should be added to the shopping cart$/, async function () {
     let findResults = await $$('.MiniCartstyles__StyledCounter-sc-1wu749y-6 cQZUCU');
     let resultTexts = [];
@@ -48,7 +88,26 @@ module.exports = function () {
     expect(resultTexts).to.not.equal(0);
   });
 
+  this.Then(/^the product count should be increased in the shopping cart$/, async function () {
+    let findResults = await $$('.MiniCartstyles__StyledCounter-sc-1wu749y-6 cQZUCU');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.not.equal(1);
+  });
+
+  this.Then(/^the product count should be decreased in the shopping cart$/, async function () {
+    let findResults = await $$('.MiniCartstyles__StyledCounter-sc-1wu749y-6 cQZUCU');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.not.equal(1);
+  });
+
 };
+
 
 
 
