6a40d7c0b213ee7c7993c0c4ca71eb07bfc381ac
Noruh <89393382+Noruh@users.noreply.github.com>
2021-10-15 10:55:16 +0200

    Added 2 scenarios to id5
    To get more test coverage. also deleted some functions that weren't in use in step-def 3 and 5

--- step-definitions/test-id-3.js
+++ step-definitions/test-id-3.js
@@ -8,16 +8,6 @@ async function waitAWhile() {
 
 module.exports = function () {
 
-  this.When(/^I enter "([^"]*)" in the search box$/, async function (searchPhrase) {
-    let searchBox = await $('input[placeholder="Sök i e-handeln"]');
-    await searchBox.sendKeys(searchPhrase);
-  });
-
-  this.When(/^press the enter key$/, async function () {
-    let searchBox = await $('input[placeholder="Sök i e-handeln"]');
-    await searchBox.sendKeys(selenium.Key.ENTER);
-  });
-
   this.When(/^I click on the plus button on a product$/, async function () {
 
     let clickButton = await $('[class^= "ax-btn ax-product-quantity-btn ax-product-quantity-plus"]');
@@ -31,21 +21,6 @@ module.exports = function () {
     await clickButton.click();
     await clickButton.click();
     await waitAWhile();
-  });
-
-  this.When(/^I should get a search result$/, async function () {
-    await driver.wait(until.elementsLocated(by.css('[class^= "Grid_grid"]')), 20000);
-    let findList = await $('[class^= "Grid_grid"]');
-    expect(findList).to.not.equal(null);
-  });
-
-  this.When(/^the search result should contain the correct product$/, async function () {
-    let findResults = await $$('[class^= "Product_product-name"]');
-    let resultTexts = [];
-    for (let findResult of findResults) {
-      resultTexts.push(await findResult.getText());
-    }
-    expect(resultTexts).to.include('Delicatoboll 1-pack');
   });
 
   this.Then(/^the product count should be increased$/, async function () {
@@ -98,7 +73,8 @@ module.exports = function () {
   });
 
   this.Then(/^there should be a text displaying that my cart is empty$/, async function () {
-    let findResults = await $$('.selenium--miniCart-empty-text');
+    await driver.sleep(1000);
+    let findResults = await $$('[class^= "selenium--miniCart-empty-text"]');
     let resultTexts = [];
     for (let findResult of findResults) {
       resultTexts.push(await findResult.getText());
--- step-definitions/test-id-5.js
+++ step-definitions/test-id-5.js
@@ -1,6 +1,6 @@
 const { $, $$ } = require('../helpers/element-selection.js');
 
-let slowDown = false;
+let slowDown = true;
 
 async function waitAWhile() {
   await driver.sleep(slowDown ? 5000 : 0);
@@ -9,33 +9,9 @@ async function waitAWhile() {
 
 module.exports = function () {
 
-  this.Given(/^there is products in the shopping cart$/, async function () {
-    let findResults = await $$('#selenium--cart-badge-total-unit-count');
-    let resultTexts = [];
-    for (let findResult of findResults) {
-      resultTexts.push(await findResult.getText());
-    }
-    expect(resultTexts).to.eql(['2']);
-  });
-
   this.Given(/^the shopping cart menu is open$/, async function () {
     let clickButton = await $('[class^= "ax-btn ax-btn-fab ax-toolbar-btn"]');
     await clickButton.click();
-  });
-
-  this.When(/^I should get the search result$/, async function () {
-    await driver.wait(until.elementsLocated(by.css('[class^= "Grid_grid"]')), 20000);
-    let findList = await $('[class^= "Grid_grid"]');
-    expect(findList).to.not.equal(null);
-  });
-
-  this.When(/^the search result should contain ciabatta$/, async function () {
-    let findResults = await $$('[class^= "Product_product-name"]');
-    let resultTexts = [];
-    for (let findResult of findResults) {
-      resultTexts.push(await findResult.getText());
-    }
-    expect(resultTexts).to.include('Ciabatta');
   });
 
   this.When(/^I click the buy-button$/, async function () {
@@ -43,13 +19,46 @@ module.exports = function () {
     await clickButton.click();
   });
 
-  this.When(/^the product is added to the shopping cart $/, async function () {
-    let findResults = await $$('.ax-cart-mini .ax-badge.ax-badge-info');
+  this.When(/^I click on the plus button on the product in the side menu$/, async function () {
+    let clickButton = await $$('[class^= "ax-btn ax-product-quantity-btn ax-product-quantity-plus"]');
+    let confirmButton = clickButton[0];
+    await confirmButton.click();
+    await waitAWhile();
+  });
+
+  this.When(/^I click on the minus button on the product in the side menu$/, async function () {
+    let clickButton = await $$('[class^= "ax-btn ax-product-quantity-btn"]');
+    let confirmButton = clickButton[0];
+    await confirmButton.click();
+    await waitAWhile();
+  });
+
+  this.Then(/^the product count should be increased in the side menu$/, async function () {
+    let findResults = await $$('#selenium--cart-badge-total-unit-count');
     let resultTexts = [];
     for (let findResult of findResults) {
       resultTexts.push(await findResult.getText());
     }
-    expect(resultTexts).to.eql(['2']);
+    expect(resultTexts).to.eql(['3']);
+    let h2Text;
+    while (h2Text !== '3') {
+      h2Text = await (await driver.findElement(By.css('#selenium--cart-badge-total-unit-count'))).getText();
+      await driver.sleep(100);
+    }
+  });
+
+  this.Then(/^the product count should be decreased in the side menu$/, async function () {
+    let findResults = await $$('#selenium--cart-badge-total-unit-count');
+    let resultTexts = [];
+    for (let findResult of findResults) {
+      resultTexts.push(await findResult.getText());
+    }
+    expect(resultTexts).to.eql(['1']);
+    let h2Text;
+    while (h2Text !== '1') {
+      h2Text = await (await driver.findElement(By.css('#selenium--cart-badge-total-unit-count'))).getText();
+      await driver.sleep(100);
+    }
   });
 
   this.Then(/^I click on the plus button on the product in the shopping cart$/, async function () {
@@ -64,19 +73,12 @@ module.exports = function () {
     await confirmButton.click();
   });
 
-  this.Then(/^I click on the plus-button on a product$/, async function () {
-    let clickButton = await $$('[class^= "CardQuantityInputField_quantity-button"]');
-    let confirmButton = clickButton[1];
-    await confirmButton.click();
-  });
-
   this.Then(/^the product count should be increased in the shopping cart$/, async function () {
     let h2Text;
     while (h2Text !== '3') {
       h2Text = await (await driver.findElement(By.css('.ax-cart-mini .ax-badge.ax-badge-info'))).getText();
       await driver.sleep(100);
     }
-
     let findResults = await $$('.ax-cart-mini .ax-badge.ax-badge-info');
     let resultTexts = [];
     for (let findResult of findResults) {
@@ -91,7 +93,6 @@ module.exports = function () {
       h2Text = await (await driver.findElement(By.css('.ax-cart-mini .ax-badge.ax-badge-info'))).getText();
       await driver.sleep(100);
     }
-
     let findResults = await $$('.ax-cart-mini .ax-badge.ax-badge-info');
     let resultTexts = [];
     for (let findResult of findResults) {
